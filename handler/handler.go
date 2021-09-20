a8091c7d0fafa925310c608b2ecb145d41a5c9c5
aaronbn <55015212+aaronbn@users.noreply.github.com>
2021-09-20 20:30:24 +0700

    Fix DB, add query for Dashboard GORM

--- handler/handler.go
+++ handler/handler.go
@@ -36,10 +36,10 @@ type Respawns struct {
 
 type Dashboard struct {
 	FullName        string  `json:"full_name"`
-	Role            string  `json:"role"`
+	RoleDescription string  `json:"role_description"`
 	OfficeLongitude float32 `json:"office_longitude"`
 	OfficeLatitude  float32 `json:"office_latitude"`
-	CurrentDate     string  `json:"current_date"`
+	CurrentDate     string  `json:"dates"`
 	UserLongitude   float32 `json:"user_longitude"`
 	UserLatitude    float32 `json:"user_latitude"`
 }
--- main.go
+++ main.go
@@ -1,28 +1,32 @@
 package main
 
 import (
+	"be-project/handler"
 	"be-project/router"
 	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-/*type Role struct {
-	ID         int    `json:"role_id"`
-	Descrption string `json:"role_description"`
-}*/
-
 func main() {
-	/*var dsn = "u5z3elx9bcxq6mf8:niaslitks0xa71a2@tcp(uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306)/iaws6lsi5kzfjyyo?charset=utf8mb4&parseTime=True&loc=Local"
+	var dsn = "u5z3elx9bcxq6mf8:niaslitks0xa71a2@tcp(uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306)/iaws6lsi5kzfjyyo?charset=utf8mb4&parseTime=True&loc=Local"
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err == nil {
 		fmt.Println("Success")
 	}
 
-	var roles []Role
-	db.Find(&roles)
-	fmt.Println(roles.ID[0])*/
+	var dashboard handler.Dashboard
+	//var roles []handler.Role
+	err = db.Raw("SELECT full_name, role_description, office_longitude, office_latitude, dates, user_longitude, user_latitude FROM users AS a JOIN roles AS b ON a.role_id = b.role_id JOIN offices AS c ON a.office_id = c.office_id JOIN attendances AS d ON a.user_id = d.user_id WHERE a.user_id = 1 ORDER BY d.dates DESC LIMIT 1;").Scan(&dashboard).Error
+	//err = db.Debug().Find(&roles).Error
+	fmt.Println("Result: ", dashboard)
+	/*for _, b := range roles {
+		fmt.Println("ID: ", b.ID)
+		fmt.Println("Roles: ", b.Description)
+	}*/
 
 	router.Router()
 }
