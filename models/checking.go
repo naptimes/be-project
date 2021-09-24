b1268f8ce21420eadba8c4d1526046e61cd701eb
naptimes <alibasaeb@gmail.com>
2021-09-25 01:43:39 +0700

    change struct name to Attendances

--- models/checking.go
+++ models/checking.go
@@ -1,8 +1,8 @@
 package models
 
-type Checking struct {
+type Attendances struct {
 	UserId        int     `json:"user_id"`
 	UserLongitude float32 `json:"user_longitude"`
 	UserLatitude  float32 `json:"user_latitude"`
-	CurrentDate   string  `json:"current_date" gorm:"colomn:dates"`
+	CurrentDate   string  `json:"current_date" gorm:"column:dates"`
 }
