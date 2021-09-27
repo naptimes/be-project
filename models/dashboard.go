e5f01f9929c56fc4f425385870ffa488cd080b35
aaronbn <55015212+aaronbn@users.noreply.github.com>
2021-09-27 21:21:55 +0700

    Add function to calculate distance

--- models/dashboard.go
+++ models/dashboard.go
@@ -3,6 +3,6 @@ package models
 type Dashboard struct {
 	FullName        string  `json:"full_name"`
 	RoleDescription string  `json:"role_description"`
-	OfficeLongitude float32 `json:"office_longitude"`
-	OfficeLatitude  float32 `json:"office_latitude"`
+	OfficeLongitude float64 `json:"office_longitude"`
+	OfficeLatitude  float64 `json:"office_latitude"`
 }
