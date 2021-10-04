13dd65df2c958c6dda17bd1500a9a2589163ecc8
aaronbn <55015212+aaronbn@users.noreply.github.com>
2021-10-04 12:50:18 +0700

    Add func to handle CORS, not tested yet

--- router/router.go
+++ router/router.go
@@ -11,14 +11,30 @@ func Router() {
 
 	api := router.Group("/api")
 
-	api.GET("/", handler.LandingPage)
-	api.GET("/dashboard", handler.GetDashboard)
-	api.GET("/timesheet", handler.GetTimesheet)
-	api.GET("/administration", handler.GetAdministration)
-	api.POST("/checkin", handler.PostCheckIn)
-	api.POST("/checkout", handler.PostCheckOut)
-	api.POST("/auth/register", handler.Register)
-	api.POST("/auth/login", handler.Login)
+	api.GET("/", CORSMiddleware(), handler.LandingPage)
+	api.GET("/dashboard", CORSMiddleware(), handler.GetDashboard)
+	api.GET("/timesheet", CORSMiddleware(), handler.GetTimesheet)
+	api.GET("/administration", CORSMiddleware(), handler.GetAdministration)
+	api.POST("/checkin", CORSMiddleware(), handler.PostCheckIn)
+	api.POST("/checkout", CORSMiddleware(), handler.PostCheckOut)
+	api.POST("/auth/register", CORSMiddleware(), handler.Register)
+	api.POST("/auth/login", CORSMiddleware(), handler.Login)
 
 	router.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
