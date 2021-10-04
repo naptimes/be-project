ded6cbcc768403e0b9eb2ba99cf84d003cb8eaf2
naptimes <alibasaeb@gmail.com>
2021-10-04 21:28:05 +0700

    add auth middleware

--- router/router.go
+++ router/router.go
@@ -2,7 +2,9 @@ package router
 
 import (
 	"be-project/handler"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,6 +21,18 @@ func Router() {
 	api.POST("/checkout", handler.Auth, handler.PostCheckOut)
 	api.POST("/auth/register", handler.Register)
 	api.POST("/auth/login", handler.Login)
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
+		AllowHeaders:     []string{"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	router.Run()
 }
