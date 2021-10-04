601ff09b76c1c30068e003d166f9ac5872247ca3
naptimes <alibasaeb@gmail.com>
2021-10-04 21:22:56 +0700

    remove commented code

--- router/router.go
+++ router/router.go
@@ -35,19 +35,3 @@ func Router() {
 	}))
 	router.Run()
 }
-
-// func CORSMiddleware() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://ipe8-workerattendance.herokuapp.com/")
-// 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-// 		if c.Request.Method == "OPTIONS" {
-// 			c.AbortWithStatus(204)
-// 			return
-// 		}
-
-// 		c.Next()
-// 	}
-// }
