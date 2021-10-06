381bc70f224b8f849a7d8bdf049f53905659ab5f
aaronbn <55015212+aaronbn@users.noreply.github.com>
2021-10-06 20:10:18 +0700

    Modification on CORS

--- router/router.go
+++ router/router.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"be-project/handler"
-	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -23,13 +22,6 @@ func Router() {
 	api.POST("/auth/register", handler.Register)
 	api.POST("/auth/login", handler.Login)
 
-	router.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"https://ipe8-workerattendance.herokuapp.com/"},
-		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "DELETE", "OPTION"},
-		AllowHeaders: []string{"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"},
-		//ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
+	router.Use(cors.Default())
 	router.Run()
 }
