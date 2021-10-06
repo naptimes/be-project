4f6a0c44bfa0554bd330062168d2fcbdf756c5b5
aaronbn <55015212+aaronbn@users.noreply.github.com>
2021-10-06 19:15:34 +0700

    Modification on CORS

--- router/router.go
+++ router/router.go
@@ -24,9 +24,10 @@ func Router() {
 	api.POST("/auth/login", handler.Login)
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
-		AllowHeaders:     []string{"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"},
+		AllowOrigins: []string{"https://ipe8-workerattendance.herokuapp.com/"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "DELETE", "OPTION"},
+		AllowHeaders: []string{"*"},
+		//AllowHeaders:     []string{"Access-Control-Allow-Headers", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
